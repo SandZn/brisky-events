a0d00b29857c1a33681534ba1de2ee33b7b30714
Jim de Beer <jim@vigour.io>
2016-07-08 11:40:06 +0200

    remove restore (get rdy for refactor)

--- test/context.js
+++ test/context.js
@@ -8,7 +8,7 @@ const trigger = require('../trigger')
 Element.prototype.inject(require('../lib'))
 
 test('context - fire events - restore context', function (t) {
-  t.plan(2)
+  t.plan(3)
   const state = s({
     clients: {
       child: {
@@ -25,16 +25,18 @@ test('context - fire events - restore context', function (t) {
 
   const cool = state.clients.child.prototype.cool
   const orig = cool.path()
-
   const app = render({
     types: {
       a: {
-        $: 'clients.a',
+        $: 'clients.a.cool',
         text: { $: 'text' },
         on: {
           mousedown (event, stamp) {
             t.equal('state' in event, true, 'has state in event')
-            t.same(event.state.cool.path(), [ 'clients', 'a', 'cool' ], 'sets context')
+            console.log(event.state.path())
+            // how to solve how to solve -- context is walker in state not in event
+            // what do we need/want
+            // dont realy no the problem where trying to solve here...
           }
         }
       }
@@ -42,9 +44,5 @@ test('context - fire events - restore context', function (t) {
     a: { type: 'a' }
   }, state)
   trigger(app.childNodes[0], 'mousedown')
-
-  console.log(cool.path(), orig)
-  // definitely wrong
-
-  console.log(p(app))
+  t.same(cool.path(), orig, 'restores previous context')
 })
