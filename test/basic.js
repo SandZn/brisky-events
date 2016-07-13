ab5358e6a5f4a0266cb5049d0be69a41b9fac06e
Jim de Beer <jim@vigour.io>
2016-07-13 16:21:36 +0200

    moved restore to a file so it can be used in fire event

--- test/basic.js
+++ test/basic.js
@@ -2,6 +2,7 @@
 const render = require('brisky-core/render')
 const Element = require('brisky-core')
 const test = require('tape')
+const p = require('parse-element')
 Element.prototype.inject(require('../lib'))
 
 test('basic - add events', function (t) {
@@ -12,5 +13,25 @@ test('basic - add events', function (t) {
   t.true(elem.hasEvents, 'adds hasEvents on element')
   t.equals(node._, elem, 'stores element on node')
   t.false('_s' in node, 'doesn\'t store state on node when no state')
+  t.end()
+})
+
+test('basic - prevent', function (t) {
+  const elem = {
+    on: { mousedown () {
+      t.fail('should be prevented')
+    } },
+    nest: {
+      on: {
+        mousedown (event) {
+          event.prevent = true
+        }
+      }
+    }
+  }
+  const app = render(elem)
+
+  console.log(p(app))
+  // app
   t.end()
 })
